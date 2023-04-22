2151ea42133ea2c4dd502cdd735ceeb00dde9445
rob_va <rob_van_aert@yahoo.com>
2023-04-22 14:47:20 +0000

    Improve seeders and create Login endpoint

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,15 +2,12 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Product;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class ProductSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         Product::factory(25)->create();
